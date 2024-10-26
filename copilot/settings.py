5972dbcc8613349c277c0ac8a570f07d77c30995
Aditya Gudimella <aditya.gudimella@microsoft.com>
2024-10-26 14:54:24 -0700

    Minor update to allow extra keys in CopilotSettings

--- copilot/settings.py
+++ copilot/settings.py
@@ -5,6 +5,6 @@ import pydantic as pdt
 import pydantic_settings as pds
 
 
-class CopilotSettings(pds.BaseSettings):
+class CopilotSettings(pds.BaseSettings, extra="allow"):
     model_config = pds.SettingsConfigDict(env_file=REPO_ROOT / ".env")
     openai_api_key: str = pdt.Field(alias="COPILOT_OPENAI_API_KEY")
